a2be649d0dff0308b345f259fea2c1628e55b0df
Max-Miller-10 <millermg@stpaulsschool.org.uk>
2025-10-06 17:27:21 +0100

    Added some code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using Systen.Text;
+
+
+
+List<string> MexicanWave(string input)
+{
+	List<string> result = new List<string>();
+	char[] chars = input.ToCharArray();
+
+	for (int i = 0; i < input.Length; i++){
+		if (chars[i] == ' '){continue;}
+		chars[i] = char.ToUpper(chars[i]);
+		result.Add(new string(chars));
+		chars[i] = input[i];
+	}
+	return result;
+}
+
+Debug.Assert(MexicanWave("hello")[2] == "heLlo");
+
+
+List<int> CharacterCodes(string input){
+	List<int> result = new List<int>();
+
+	foreach (char c in input){
+		result.Add((int)c);
+	}
+	return result;
+}
+
+
+string StringFromCharCodes(List<int> input)
+{
+	StringBuilder sb = new StringBuilder();
+
+
+	foreach (int code in input){
+		sb.Append((char)code);
+	}
+
+	return sb.ToString();
+}
+
+
